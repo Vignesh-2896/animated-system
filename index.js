bbc8db415b4eb61c8f301c732fcb0e9fdb70f4d6
Vignesh <vigneshb2896@gmail.com>
2024-09-28 22:55:05 +0530

    Formattting.

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-var http = require("http");
-var fs = require("fs");
+let http = require("http");
+let fs = require("fs");
 
-const server = http.createServer((req,res) => {
-    let urlType = req.url.slice(1);
-
-    res.writeHead(200, {'Content-Type':"text/html"});
-    if(urlType == "") writeData(res,"index.html")
-    else if(urlType == "about") writeData(res,"about.html");
-    else if(urlType == "contact") writeData(res,"contact.html")
-    else writeData(res,"404.html")
+const server = http.createServer((req, res) => {
+  let urlType = req.url.slice(1);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  if (urlType == "") writeData(res, "index.html");
+  else if (urlType == "about") writeData(res, "about.html");
+  else if (urlType == "contact") writeData(res, "contact.html");
+  else writeData(res, "404.html");
 });
 
 server.listen(8080);
 
 const fetchDataSync = (fileName) => {
-    return fs.readFileSync(fileName,"utf8");
-}
+  return fs.readFileSync(fileName, "utf8");
+};
 
-const writeData = (response,fileName) => {
-    htmlData = fetchDataSync(fileName);
-    response.write(htmlData);
-    response.end();
-}
+const writeData = (response, fileName) => {
+  let htmlData = fetchDataSync(fileName);
+  response.write(htmlData);
+  response.end();
+};
